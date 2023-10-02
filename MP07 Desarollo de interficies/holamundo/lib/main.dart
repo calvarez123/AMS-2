c526e075d689c64ed9173cb8a7a577f594c148d1
cristian <calvarezmartinez.cf@iesesteveterrades.com>
2023-10-02 18:29:40 +0200

    hola mundo

---
+++ MP07 Desarollo de interficies/holamundo/lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      theme: CupertinoThemeData(brightness: Brightness.dark),
+      home: MyHomePage(
+        title: 'Flutter Cupertino Demo',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text(widget.title),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Icon(
+                    CupertinoIcons.bell_fill,
+                    color: Color(0xFF00AA00),
+                    size: 25.0,
+                  ),
+                  const Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    ' $_counter',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding:
+                  EdgeInsets.all(20.0), // Ajusta el espacio alrededor del botón
+              child: CupertinoButton.filled(
+                onPressed: _incrementCounter,
+                child: const Text('Incrementar'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
