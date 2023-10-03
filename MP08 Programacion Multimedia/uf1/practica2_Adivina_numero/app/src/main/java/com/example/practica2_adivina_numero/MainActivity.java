b1c3c2a3dafc04909b585503eca1033ad194d333
cristian <calvarezmartinez.cf@iesesteveterrades.com>
2023-10-03 17:39:15 +0200

    practica juego

--- MP08 Programacion Multimedia/uf1/practica2_Adivina_numero/app/src/main/java/com/example/practica2_adivina_numero/MainActivity.java
+++ MP08 Programacion Multimedia/uf1/practica2_Adivina_numero/app/src/main/java/com/example/practica2_adivina_numero/MainActivity.java
@@ -4,20 +4,62 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.practica2_adivina_numero.RankingActivity;
+
+import java.util.ArrayList;
 import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
     private int intentosnumero = 0;
     private int aleatorio = 0;
+    static ArrayList<Ranking> rankings;
+    class Ranking {
+        private String name;
+        private int intentos;
+        private int numero;
+
+        public Ranking(String name, int intentos, int numero) {
+            this.name = name;
+            this.intentos = intentos;
+            this.numero = numero;
+        }
+
+        // Métodos getter
+        public String getName() {
+            return name;
+        }
+
+        public int getIntentos() {
+            return intentos;
+        }
+
+        public int getNumero() {
+            return numero;
+        }
+
+        // Métodos setter
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setIntentos(int intentos) {
+            this.intentos = intentos;
+        }
+
+        public void setNumero(int numero) {
+            this.numero = numero;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,33 +67,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        rankings = new ArrayList<Ranking>();
+
+
         int duration = Toast.LENGTH_SHORT;
 
         Random r = new Random();
         generarAleatorio();
 
+        Intent intent = new Intent(MainActivity.this, RankingActivity.class);
+
+
 
         final EditText editText = findViewById(R.id.textousu);
-        final TextView intentos = findViewById(R.id.intentos);
+        final TextView intentos = findViewById(R.id.intentos);class Ranking {
+            private String name;
+            private int intentos;
+            private int numero;
+
+            public Ranking(String name, int intentos, int numero) {
+                this.name = name;
+                this.intentos = intentos;
+                this.numero = numero;
+            }
+
+            // Métodos getter
+            public String getName() {
+                return name;
+            }
+
+            public int getIntentos() {
+                return intentos;
+            }
+
+            public int getNumero() {
+                return numero;
+            }
+
+            // Métodos setter
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public void setIntentos(int intentos) {
+                this.intentos = intentos;
+            }
+
+            public void setNumero(int numero) {
+                this.numero = numero;
+            }
+
+        }
         final TextView textoscroll = findViewById(R.id.scrolltext);
         final Button button = findViewById(R.id.button1);
 
-        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE || event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
-
-                    button.performClick();
-                    return true;
-                }
-                return false;
-            }
-        });
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 intentosnumero = intentosnumero + 1;
                 intentos.setText("Intentos = "+ intentosnumero);
 
@@ -62,17 +137,22 @@ public class MainActivity extends AppCompatActivity {
                     if (numeroEntero == aleatorio){
                         Toast.makeText(MainActivity.this, "Adivinado", duration).show();
                         textoscroll.append("El numero era "+aleatorio+"\n");
-                        mostrarGanaste(intentosnumero);
+                        mostrarGanaste(intentosnumero,intent);
                         generarAleatorio();
                         intentosnumero = 0;
+                        Ranking jugador = new Ranking("nombre",intentosnumero,aleatorio);
+
                         editText.setText("");
+
                     } else {
                         if (numeroEntero < aleatorio){
                             textoscroll.append("El numero es mayor que "+numeroEntero+"\n");
+                            editText.setText("");
                             Toast.makeText(MainActivity.this, "El numero es mayor que "+numeroEntero, duration).show();
                         } else if (numeroEntero > aleatorio){
                             textoscroll.append("El numero es menor que "+numeroEntero+"\n");
                             Toast.makeText(MainActivity.this, "El numero es menor que "+numeroEntero , duration).show();
+                            editText.setText("");
                         }
                     }
                 } catch (NumberFormatException e) {
@@ -86,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void mostrarGanaste(int intentosnumero) {
+    public void mostrarGanaste(int intentosnumero,Intent intent) {
         // Crear un constructor de AlertDialog
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
@@ -97,7 +177,8 @@ public class MainActivity extends AppCompatActivity {
         // Configurar botones y acciones
         builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                // Código a ejecutar cuando se hace clic en el botón "Aceptar"
+
+
                 dialog.dismiss(); // Cerrar el cuadro de diálogo
             }
         });
@@ -110,5 +191,20 @@ public class MainActivity extends AppCompatActivity {
     public void generarAleatorio() {
         Random random = new Random();
         aleatorio = random.nextInt(100) + 1;
+    }
+
+    public static class RankingActivity1 extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_ranking1);
+
+            // Obtén una referencia al TextView en tu diseño (layout)
+            TextView textView = findViewById(R.id.textView);
+
+            // Configura el texto del TextView con "Hola, mundo"
+            textView.setText("Hola, mundo");
+        }
     }
 }
