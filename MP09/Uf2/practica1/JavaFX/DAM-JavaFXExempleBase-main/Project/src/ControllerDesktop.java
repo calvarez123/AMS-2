42112af1734037117c2e70ca8b8e79cf82ec35ac
calvarez123 <calvarezmartinez.cf@iesesteveterradas.cat>
2023-10-04 15:54:47 +0200

    practicam9

--- MP09/Uf2/practica1/JavaFX/DAM-JavaFXExempleBase-main/Project/src/ControllerDesktop.java
+++ MP09/Uf2/practica1/JavaFX/DAM-JavaFXExempleBase-main/Project/src/ControllerDesktop.java
@@ -5,9 +5,12 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
@@ -35,39 +38,60 @@ public class ControllerDesktop implements Initializable {
     }
     public void loadList() {
 
-        // Obtenir l'opció seleccionada
-        String opcio = ChoiceBox.getValue();
+      // Obtenir l'opció seleccionada
+      String opcio = ChoiceBox.getValue();
+  
+      // Obtenir una referència a AppData que gestiona les dades
+      AppData appData = AppData.getInstance();
+  
+      // Mostrar el missatge de càrrega
+      showLoading();
+  
+      // Demanar les dades
+      appData.load(opcio, (result) -> {
+        if (result == null) {
+          System.out.println("ControllerDesktop: Error loading data.");
+        } else {
+          // Cal afegir el try/catch a la crida de ‘showList’
+          try {
+            showList();
+          } catch (Exception e) {
+            System.out.println("ControllerDesktop: Error showing list.");
+          }
+        }
+      });
+      
+      }
+  
+
+
+
+
+
+      // Funció ‘showLoading’, mostrar una càrrega
+
+      public void showLoading() {
+
+        // Esborrar la llista actual
+        yPane.getChildren().clear();
+        
+        // Afegeix un indicador de progrés com a primer element de la llista
+        ProgressIndicator progressIndicator = new ProgressIndicator();
+        yPane.getChildren().add(progressIndicator);
+        }
+  
+
+
+      public void showList() throws Exception {
+
+        String opcioSeleccionada = ChoiceBox.getValue();
     
-        // Obtenir una referència a AppData que gestiona les dades
+        // Obtenir una referència a l'ojecte AppData que gestiona les dades
         AppData appData = AppData.getInstance();
     
-        // Mostrar el missatge de càrrega
-        showLoading();
-    
-        // Demanar les dades
-        appData.load(opcio, (result) -> {
-          if (result == null) {
-              System.out.println("ControllerDesktop: Error loading.");
-            } else {
-              showList(opcio);
-            }
-          });
-        }
-    public void showList(String opcioCarregada) throws Exception {
-
-    // Si s'ha carregat una altra opció, no cal fer res
-    // (perquè el callback pot arribar després de que l'usuari hagi canviat d'opció)
-    String opcioSeleccionada = ChoiceBox.getValue();
-    if (opcioCarregada.compareTo(opcioSeleccionada) != 0) {
-      return;
-    }
-
-    // Obtenir una referència a l'ojecte AppData que gestiona les dades
-    AppData appData = AppData.getInstance();
-
-    // Obtenir les dades de l'opció seleccionada
-    JSONArray dades = appData.getData(opcioCarregada);
-    // Esborrar la llista actual
+        // Obtenir les dades de l'opció seleccionada
+        JSONArray dades = appData.getData(opcioSeleccionada);
+        // Esborrar la llista actual
     yPane.getChildren().clear();
 
     // Carregar la llista amb les dades
@@ -79,6 +103,32 @@ public class ControllerDesktop implements Initializable {
         yPane.getChildren().add(label);
       }
     }
+    // Funció ‘showList’, modificar com es mostren les dades
+
+// Carregar la plantilla
+URL resource = this.getClass().getResource("assets/template_list_item.fxml");
+
+// Esborrar la llista actual
+yPane.getChildren().clear();
+
+// Carregar la llista amb les dades
+for (int i = 0; i < dades.length(); i++) {
+  JSONObject consoleObject = dades.getJSONObject(i);
+
+  if (consoleObject.has("nom")) {
+    String nom = consoleObject.getString("nom");
+    String imatge = "assets/images/" + consoleObject.getString("imatge");
+    FXMLLoader loader = new FXMLLoader(resource);
+    Parent itemTemplate = loader.load();
+    ControllerListItem itemController = loader.getController();
+    itemController.setText(nom);
+    itemController.setImage(imatge);
+
+    yPane.getChildren().add(itemTemplate);
+  }
+}
+
   }
 
-}
+};
+
