025d6be03e5e76a9a08aad44defba563c3dd446d
cristian <calvarezmartinez.cf@iesesteveterrades.com>
2023-10-10 17:36:06 +0200

    androiddeveloper

--- MP08 Programacion Multimedia/uf1/practica2_Adivina_numero/app/src/main/java/com/example/practica2_adivina_numero/MainActivity.java
+++ MP08 Programacion Multimedia/uf1/practica2_Adivina_numero/app/src/main/java/com/example/practica2_adivina_numero/MainActivity.java
@@ -59,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
             this.numero = numero;
         }
 
+
+
     }
 
     @Override
@@ -67,7 +69,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
+
         rankings = new ArrayList<Ranking>();
+
+        Ranking ranking1 = new Ranking("Usuario1", 10, 42);
+        Ranking ranking2 = new Ranking("Usuario2", 8, 56);
+        Ranking ranking3 = new Ranking("Usuario3", 15, 29);
+        Ranking ranking4 = new Ranking("Usuario4", 12, 73);
+        Ranking ranking5 = new Ranking("Usuario5", 9, 64);
+
+        rankings.add(ranking1);
+        rankings.add(ranking2);
+        rankings.add(ranking3);
+        rankings.add(ranking4);
+        rankings.add(ranking5);
 
 
         int duration = Toast.LENGTH_SHORT;
@@ -80,46 +96,16 @@ public class MainActivity extends AppCompatActivity {
 
 
         final EditText editText = findViewById(R.id.textousu);
-        final TextView intentos = findViewById(R.id.intentos);class Ranking {
-            private String name;
-            private int intentos;
-            private int numero;
-
-            public Ranking(String name, int intentos, int numero) {
-                this.name = name;
-                this.intentos = intentos;
-                this.numero = numero;
-            }
-
-            // Métodos getter
-            public String getName() {
-                return name;
-            }
-
-            public int getIntentos() {
-                return intentos;
-            }
-
-            public int getNumero() {
-                return numero;
-            }
-
-            // Métodos setter
-            public void setName(String name) {
-                this.name = name;
-            }
-
-            public void setIntentos(int intentos) {
-                this.intentos = intentos;
-            }
-
-            public void setNumero(int numero) {
-                this.numero = numero;
-            }
-
-        }
+        final TextView intentos = findViewById(R.id.intentos);
         final TextView textoscroll = findViewById(R.id.scrolltext);
         final Button button = findViewById(R.id.button1);
+        final Button button2 = findViewById(R.id.pantalla2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
 
 
 
@@ -137,10 +123,10 @@ public class MainActivity extends AppCompatActivity {
                     if (numeroEntero == aleatorio){
                         Toast.makeText(MainActivity.this, "Adivinado", duration).show();
                         textoscroll.append("El numero era "+aleatorio+"\n");
-                        mostrarGanaste(intentosnumero,intent);
+                        mostrarGanaste(intentosnumero);
                         generarAleatorio();
                         intentosnumero = 0;
-                        Ranking jugador = new Ranking("nombre",intentosnumero,aleatorio);
+
 
                         editText.setText("");
 
@@ -166,45 +152,66 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void mostrarGanaste(int intentosnumero,Intent intent) {
+    public void mostrarGanaste(int intentosnumero) {
         // Crear un constructor de AlertDialog
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         // Configurar el título y el mensaje
         builder.setTitle("Felicidades")
-                .setMessage("Has adivinado el numero con solo "+intentosnumero+" intentos.");
+                .setMessage("Has adivinado el número con solo " + intentosnumero + " intentos.")
+                .setCancelable(false); // Evitar que el diálogo se cierre tocando fuera de él
 
-        // Configurar botones y acciones
-        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+        // Agregar botón "Guardar score" (sí)
+        builder.setPositiveButton("Guardar score", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
+                // Aquí puedes agregar el código para permitir al usuario ingresar su nombre
+                // y guardar el score si selecciona "sí"
 
+                // Crear un cuadro de diálogo personalizado para ingresar el nombre
+                AlertDialog.Builder nameDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                nameDialogBuilder.setTitle("Ingresa tu nombre");
 
+                final EditText input = new EditText(MainActivity.this);
+                nameDialogBuilder.setView(input);
+
+                nameDialogBuilder.setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String nombre = input.getText().toString();
+                        Ranking jugador = new Ranking(nombre,intentosnumero,aleatorio);
+                        rankings.add(jugador);
+
+                        // Aquí puedes guardar el nombre y el score
+                        // Puedes usar SharedPreferences o cualquier otra forma de almacenamiento de datos
+                    }
+                });
+
+                nameDialogBuilder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                nameDialogBuilder.show();
+            }
+        });
+
+        // Agregar botón "Aceptar" (no)
+        builder.setNegativeButton("Aceptar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
                 dialog.dismiss(); // Cerrar el cuadro de diálogo
             }
         });
 
-
-        // Mostrar el AlertDialog
-        AlertDialog dialog = builder.create();
-        dialog.show();
+        // Mostrar el cuadro de diálogo
+        builder.show();
     }
     public void generarAleatorio() {
         Random random = new Random();
-        aleatorio = random.nextInt(100) + 1;
+        //aleatorio = random.nextInt(100) + 1;
+        aleatorio = 3;
     }
 
-    public static class RankingActivity1 extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_ranking1);
-
-            // Obtén una referencia al TextView en tu diseño (layout)
-            TextView textView = findViewById(R.id.textView);
-
-            // Configura el texto del TextView con "Hola, mundo"
-            textView.setText("Hola, mundo");
-        }
-    }
 }
--- MP08 Programacion Multimedia/uf1/practica2_Adivina_numero/app/src/main/java/com/example/practica2_adivina_numero/RankingActivity.java
+++ MP08 Programacion Multimedia/uf1/practica2_Adivina_numero/app/src/main/java/com/example/practica2_adivina_numero/RankingActivity.java
@@ -1,6 +1,10 @@
 package com.example.practica2_adivina_numero;
+import static com.example.practica2_adivina_numero.MainActivity.rankings;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,15 +24,61 @@ import java.util.Random;
 
 public class RankingActivity extends AppCompatActivity {
 
+    private TableLayout tableLayout;
+    private ArrayList<MainActivity.Ranking> rankings = MainActivity.rankings;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
+        Intent intent = new Intent(this, MainActivity.class);
+        // Obtén una referencia al TableLayout y a la ArrayList de Rankings
+        tableLayout = findViewById(R.id.tableLayout);
 
-        // Obtén una referencia al TextView en tu diseño (layout)
-        TextView textView = findViewById(R.id.textView);
+        // Llena el TableLayout con los datos de la ArrayList de Rankings
+        llenarTabla();
 
-        // Configura el texto del TextView con "Hola, Cristian"
-        textView.setText("Hola, Cristian");
+        final Button button2 = findViewById(R.id.atras);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+    private void llenarTabla() {
+        for (MainActivity.Ranking ranking : rankings) {
+            TableRow row = new TableRow(this);
+
+            TextView textViewNombre = new TextView(this);
+            textViewNombre.setText(ranking.getName());
+
+            TextView textViewIntentos = new TextView(this);
+            textViewIntentos.setText(String.valueOf(ranking.getIntentos()));
+
+            TextView textViewNumero = new TextView(this);
+            textViewNumero.setText(String.valueOf(ranking.getNumero()));
+
+            // Personaliza los TextView según tus necesidades
+            textViewNombre.setTextSize(16);
+            textViewIntentos.setTextSize(16);
+            textViewNumero.setTextSize(16);
+
+
+            textViewNombre.setPadding(0, 0, 16, 16);
+            textViewIntentos.setPadding(0, 0, 16, 16);
+            textViewNumero.setPadding(0, 0, 16, 16);
+
+            row.addView(textViewNombre);
+            row.addView(textViewIntentos);
+            row.addView(textViewNumero);
+
+            tableLayout.addView(row);
+        }
     }
 }
