900f33e9e96516a2e391638c028f49fbc272ba9e
cristian <calvarezmartinez.cf@iesesteveterrades.com>
2023-10-19 18:26:04 +0200

    asdasda

--- MP09/Uf2/threads/src/main/java/com/project/Controller0.java
+++ MP09/Uf2/threads/src/main/java/com/project/Controller0.java
@@ -1,5 +1,6 @@
 package com.project;
 
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import javafx.application.Platform;
@@ -8,18 +9,23 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.AnchorPane;
 
 public class Controller0 {
+    Random random = new Random();
+        
 
     @FXML
-    private Button button0, button1;
+    private Button button0, button1, button2, button3;
     @FXML
     private AnchorPane container;
     @FXML
-    private Label percentatge0, percentatge1;
+    private Label percentatge0, percentatge1 , percentatge2;
+    @FXML
+    private ProgressBar progress0, progress1 , progress2;
     
-    private ExecutorService executor = Executors.newFixedThreadPool(2); // Creem una pool de dos fils
+    private ExecutorService executor = Executors.newFixedThreadPool(3); // Creem una pool de dos fils
 
     @FXML
     private void animateToView1(ActionEvent event) {
@@ -28,33 +34,69 @@ public class Controller0 {
 
     @FXML
     private void runTask() {
-
+        
         backgroundTask(0);
         backgroundTask(1);
+        backgroundTask(2);
     }
 
     private void backgroundTask(int index) {
         // Executar la tasca
         executor.submit(() -> {
+            
             try {
                 for (int i = 0; i <= 100; i++) {
                     final int currentValue = i;
-    
+                    // linea uno
+
                     if (index == 0) {
                         // Actualitzar el Label en el fil d'aplicació de l'UI
                         Platform.runLater(() -> {
+                            String porcentaje0 = percentatge0.getText();
                             percentatge0.setText(String.valueOf(currentValue) + "%");
+                            
+                            progress0.setProgress(currentValue/100.0);
+                            if (porcentaje0 == "100%"){
+                                stopExecutor();
+                            }
+                            
                         });
-                        Thread.sleep(20);
+                        Thread.sleep(100);
 
                     }
-
+                    //linea 2
                     if (index == 1) {
                         // Actualitzar el Label en el fil d'aplicació de l'UI
+                        String porcentaje1 = percentatge0.getText();
+
+                        int randomNumber = random.nextInt(3) + 2; 
+                        int numeroAleatorio = (random.nextInt(3) + 3);
+                        numeroAleatorio = numeroAleatorio*1000;
+                        System.out.println(randomNumber);
+                        int numero = currentValue+randomNumber;
+                        
                         Platform.runLater(() -> {
-                            percentatge1.setText(String.valueOf(currentValue) + "%");
+                            percentatge1.setText(String.valueOf(numero) + "%");
+                            progress1.setProgress(numero/100.0);
+                            if (porcentaje1 == "100%"){
+                                stopExecutor();
+                            }
+
                         });
-                        Thread.sleep(40);
+                        Thread.sleep(numeroAleatorio);
+                    }
+
+                    if (index == 2) {
+                        // Actualitzar el Label en el fil d'aplicació de l'UI
+                        Platform.runLater(() -> {
+                            String porcentaje2 = percentatge0.getText();
+                            percentatge2.setText(String.valueOf(currentValue) + "%");
+                            progress2.setProgress(currentValue/100.0);
+                            if (porcentaje2 == "100%"){
+                                stopExecutor();
+                            }
+                        });
+                        Thread.sleep(1000);
                     }
 
                     System.out.println("Updating label: " + index + ", Value: " + currentValue);
